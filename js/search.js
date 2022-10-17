6b78a96f3741725c3bce37475f8fc5c5bc419941
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-10-17 16:40:56 -0500

    perchance

--- js/search.js
+++ js/search.js
@@ -7,15 +7,25 @@ function startSearch() {
   li = ul.getElementsByTagName("a");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("h1")[0];
-    b = a.getElementsByClassName("badge")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
-    } else if ((dropDown.value == "new") && (b.innerHTML == "New")) {
+    } else {
       li[i].style.display = "none";
-    } else if ((dropDown.value == "hot") && (b.innerHTML == "Hot")) {
-      li[i].style.display = "none";
-    } else if (dropDown.value == "n/a") {
+    }
+    
+    if (dropDown.value == "n/a") {
+      continue; //if no selection, disregard everything below this line in the for loop
+    }
+    
+    b = a.getElementsByClassName("badge");
+    if ((b ? b[0].innerHTML.includes("New") : 0) && (dropDown.value == "new")) {
       li[i].style.display = "";
+    } else if ((b ? b[0].innerHTML.includes("Hot") : 0) && (dropDown.value == "hot")) {
+      li[i].style.display = "";
+    } else if (b.length == 0) && (dropDown.value == "n/a")) {
+      li[i].style.display = "";
+    } else {
+      console.log("this will probably break lol");
     }
   }
 }
