47f90c63cd8fcaa1e5365449781c2cc5ebcc424d
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-10-17 11:17:19 -0500

    umugus
    first draft and I can't immediately test it until vercel deploys it
    which takes a hot minute

--- js/search.js
+++ js/search.js
@@ -1,15 +1,21 @@
 function startSearch() {
-  var input, filter, ul, li, a, i;
+  var input, filter, ul, li, a, i, b, dropDown;
   input = document.getElementById("mySearch");
+  dropDown = document.getElementById("select");
   filter = input.value.toUpperCase();
   ul = document.getElementById("search");
   li = ul.getElementsByTagName("a");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("h1")[0];
+    b = a.getElementsByClassName("badge")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
-    } else {
+    } else if ((dropDown.value == "new") && (b.innerHTML == "New")) {
       li[i].style.display = "none";
+    } else if ((dropDown.value == "hot") && (b.innerHTML == "Hot")) {
+      li[i].style.display = "none";
+    } else if (dropDown.value == "n/a") {
+      li[i].style.display = "";
     }
   }
 }
