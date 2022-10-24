cb3a79d27d2c233587e5daa72bee5a3bf1427436
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-10-24 09:28:50 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
 function startSearch() {
-  var input, filter, ul, li, a, i, b, dropDown;
+  var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
-  dropDown = document.getElementById("select");
   filter = input.value.toUpperCase();
   ul = document.getElementById("search");
   li = ul.getElementsByTagName("a");
@@ -11,21 +10,6 @@ function startSearch() {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
-    }
-    
-    if (dropDown.value == "n/a") {
-      continue; //if no selection, disregard everything below this line in the for loop
-    }
-    
-    b = a.getElementsByClassName("badge");
-    if ((b ? b[0].innerHTML.includes("New") : 0) && (dropDown.value == "new")) {
-      li[i].style.display = "";
-    } else if ((b ? b[0].innerHTML.includes("Hot") : 0) && (dropDown.value == "hot")) {
-      li[i].style.display = "";
-    } else if (b.length == 0) && (dropDown.value == "n/a")) {
-      li[i].style.display = "";
-    } else {
-      console.log("this will probably break lol");
     }
   }
 }
